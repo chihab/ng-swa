b1bcc0eacd4e5f89361d0f7b92a3e3d145483c0e
chihab <chihab@gmail.com>
2021-05-07 07:53:26 +0000

    feat: thank wassim and team

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Merci Wassim et la Team SWA!</div>`,
 })
-export class AppComponent {
-  value = 'World';
-}
+export class AppComponent {}
